622b8df729ab3d21f70d0e2fd2bf9d1bb3041c1d
nu11secur1ty <venvaropt@gmail.com>
2019-08-23 20:02:31 +0300

    Update installbdfactory.sh

--- installbdfactory.sh
+++ installbdfactory.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
+git clone https://github.com/nu11secur1ty/m1tmfN.git
 mkdir -p /usr/share/mitmf/bdfactory
-  cd bdfactory_module/
+  cd m1tmfN/bdfactory_module
     cp -r * /usr/share/mitmf/bdfactory/
   cd /usr/share/mitmf/bdfactory/
     ./install.sh
